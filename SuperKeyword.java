607dec8981335bc27b0c367ef3726d40db48a1b1
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-01-16 00:57:12 +0530

    Add files via upload
    The Super keyword is used to access everything non-private of a base class from a derived class.

---
+++ SuperKeyword.java
@@ -0,0 +1,59 @@
+import java.lang.*;
+
+class Base
+{
+    public int i;
+    public int j;
+
+    //Parametarized Constructor of Base Class
+    public Base(int a,int b)
+    {
+        this.i=a;
+        this.j=b;
+    }
+    public void Fun()
+    {
+        System.out.println("Inside Base Class --> Fun ()");
+    }
+
+}
+class Derived extends Base
+{
+
+    public int x;
+    public int y;
+    
+    //Constructor of Derived Class
+    public Derived()
+    {
+
+        super(11, 21);
+        
+        System.err.println("Inside Dervied Constructor");
+        
+        this.x=30;
+        this.y=40;
+    }
+
+    public void Sun()
+    {
+        System.err.println("Inside Derived -->> Sun()");
+        System.out.println(super.i);
+        super.Fun();
+    }
+
+}
+
+
+public class SuperKeyword 
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Inside Main");
+        
+        //Creating the object of Derived class
+        Derived dobj= new Derived();
+        dobj.Sun();
+    }
+    
+}
